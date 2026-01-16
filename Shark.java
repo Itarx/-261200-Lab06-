ef32db3d563632db37aa0a34f71494b44483cefc
Itarx <natthapong_nar@cmu.ac.th>
2026-01-16 16:23:37 +0700

    Create Shark.java

---
+++ Shark.java
@@ -0,0 +1,10 @@
+ class Shark extends Fish {
+     @Override
+     public void swim() {
+         System.out.println("Shark is swimming fast!");
+     }
+
+     public void eat(Fish fish) {
+         System.out.println(this.toString() + " is eating " + fish.toString());
+     }
+ }
